4d8eb9df64da624b26c0309cba13afedbef8adc4
Arslan Ablikim <arslan.ablikim0@gmail.com>
2025-09-21 15:48:08 +0800

    fix: Correct score percentage calculation in ParticipationInfo component

--- app/components/ParticipationInfo/index.tsx
+++ app/components/ParticipationInfo/index.tsx
@@ -98,7 +98,7 @@ export default function ParticipationInfo({
               <div className="relative h-3 w-full rounded-full bg-gray-800">
                 <div
                   className="absolute top-0 left-0 h-full rounded-full bg-gradient-to-r from-[#8080DA] to-[#1BCFDE]"
-                  style={{ width: `${(campaignUser?.score ?? 0) / 300}%` }}
+                  style={{ width: `${(campaignUser?.score ?? 0) / 3}%` }}
                 />
               </div>
 
