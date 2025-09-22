dd7fd1d0b23e3477a2cc81e9efef0dd99e7ac1c4
Arslan Ablikim <arslan.ablikim0@gmail.com>
2025-09-22 23:20:08 +0800

    style: adjust layout and alignment in ConnectWallet component

--- app/components/ConnectWallet/index.tsx
+++ app/components/ConnectWallet/index.tsx
@@ -70,7 +70,7 @@ export default function ConnectWallet({
   };
 
   return (
-    <div className="my-6 flex cursor-pointer items-center gap-4 border border-[#f2edea]/40 p-2 font-light max-md:bg-[#161616] md:p-5">
+    <div className="my-6 flex items-center gap-4 border border-[#f2edea]/40 p-2 font-light max-md:bg-[#161616] md:p-5">
       <img alt="airdrop-wallet" src={walletIcon} />
       <div className="text-[#c5c5c5] text-xl">
         {currentAddress
@@ -78,7 +78,7 @@ export default function ConnectWallet({
           : 'Connect Wallet'}
       </div>
       <button
-        className="ml-auto text-white underline decoration-dashed"
+        className="flex flex-1 justify-end text-white underline decoration-dashed"
         onClick={handleClick}
         type="button"
       >
